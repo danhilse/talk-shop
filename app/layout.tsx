90208038f9972995d33581809add2751536784b2
Echo <danhilse@gmail.com>
2026-01-30 10:04:25 -0700

    Add Google Tag Manager integration to layout for enhanced tracking capabilities. Include script and noscript tags for improved analytics support.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata, Viewport } from "next";
 import { Geist_Mono, DM_Sans, Instrument_Serif } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Nav } from "@/components/nav";
 import { Footer } from "@/components/footer";
 import { CookieConsent } from "@/components/cookie-consent";
+
+const GTM_ID = "GTM-5L3JGJQ2";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -90,9 +93,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${dmSans.variable} ${instrumentSerif.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Nav />
         {children}
         <Footer />
